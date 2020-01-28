8904f2fbcf890546c8040529a742720ee6f2cf63
Gene Campbell III <gcampbell391@gmail.com>
2020-01-28 00:57:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function removeLastKitten(){
   array1.pop()
   return array1
 }
-function apendKitten(name){
+function appendKitten(name){
   var array1 = []
   array1 = array1.concat(kittens)
   array1.push(name)
